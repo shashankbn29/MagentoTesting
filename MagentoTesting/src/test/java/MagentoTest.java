9a73025441fc2bbc2ebcfd2ee730464ab07ffde1
Nithya <Nithya@LAPTOP-LTLBT54P>
2019-03-03 23:48:57 +0530

    commit4

--- MagentoTesting/src/test/java/MagentoTest.java
+++ MagentoTesting/src/test/java/MagentoTest.java
@@ -10,7 +10,7 @@ public class MagentoTest {
 	@Test	
 	public void positive1()
 	{
-		FirefoxDriver driver=new FirefoxDriver();
+		ChromeDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.get("https://www.magento.com");
